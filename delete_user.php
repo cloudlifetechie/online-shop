0ec562302c784529e41f8935338ffb4d423f99f2
gopi musunuri <cloudlifetechie@gmail.com>
2025-03-18 09:35:41 +0000

    php application

--- delete_user.php
+++ delete_user.php
@@ -1,16 +1,5 @@
 <?php
-$servername = "localhost";
-$username = "veg_user";
-$password = "P@ssword1";
-$dbname = "user_registration";
-
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
+include('db.php'); // Include database connection file
 
 // Check if an ID is provided for deletion
 if (isset($_GET['id'])) {
--- register_user.php
+++ register_user.php
@@ -1,16 +1,5 @@
 <?php
-$servername = "localhost";
-$username = "veg_user";
-$password = "P@ssword1";
-$dbname = "user_registration";
-
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
+include('db.php'); // Include database connection file
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $name = $_POST['name'];
